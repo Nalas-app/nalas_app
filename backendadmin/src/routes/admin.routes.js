b3ac0ea1046726fcb2e26fcbb32d5b3ff1e86c7b
Sriya Santoshi <sriyasantoshi005@gmail.com>
2026-02-24 22:18:25 +0530

    Revert "Frontendadmin"

--- backendadmin/src/routes/admin.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginAdmin } = require("../controllers/admin.controller");
-
-router.post("/login", loginAdmin);
-
-module.exports = router;
--- frontendadmin/src/utils/token.ts
+++
@@ -1,6 +0,0 @@
-import jwtDecode from "jwt-decode";
-
-export const isTokenExpired = (token: string) => {
-  const decoded: any = jwtDecode(token);
-  return decoded.exp * 1000 < Date.now();
-};
